aa5e26cf952a9ace007872a520cd37a666520a45
axerex12 <axel.aaldijk12@gmail.com>
2024-09-03 10:13:30 +0300

    initial

--- module5/5.1.py
+++ module5/5.1.py
@@ -1,10 +1,10 @@
 import random
-from random import randint
+
 
 nopat=[]
 noppia = int(input("Montako noppaa!: "))
 for i in range(0, noppia):
-    nopat.append(randint (1,6))
+    nopat.append(random.randint (1,6))
 noppasumma = sum(nopat)
 print(noppasumma)
 print (nopat)
